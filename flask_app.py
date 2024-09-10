cb5d9842da2a335ae71490676d1be7952f7871cc
Jordy Van Haesebroucke <burny@outlook.be>
2024-09-10 17:31:24 +0200

    Les 11 Huiswerk

---
+++ flask_app.py
@@ -0,0 +1,9 @@
+from flask import Flask
+
+app = Flask(__name__)
+if __name__ == '__main__':
+    app.run(port=5000,debug=True)
+
+@app.route('/')
+def home():
+    return "<p>Mijn Droom om door te groeien als Full-Stack Developer is dichterbij aan het komen!</p>"
